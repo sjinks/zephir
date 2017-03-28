8bcc9d44853facae65a22ab2f10649b5bfa5ff6a
Volodymyr Kolesnykov <volodymyr@wildwolf.name>
2017-03-29 02:09:21 +0300

    llvm-cov is too old

--- unit-tests/ci/after_success.sh
+++ unit-tests/ci/after_success.sh
@@ -5,20 +5,18 @@ PREFIX=$(readlink -enq "$(dirname $0)/../../")
 if [ "$CC" = "gcc" -o "$CC" = "ccache gcc" -o "x$CC" = "x" ]; then
 	GCOV=gcov
 	ARGS="-ablp"
-else
-	GCOV=llvm-cov
-	ARGS="gcov -a -b -l -p"
+
+	rm -rf   "$PREFIX/.gcov"
+	mkdir -p "$PREFIX/.gcov"
+
+	DIRS=$(find "$PREFIX" -type f -name \*.gcno -printf "%h\n" | sort -u)
+
+	for i in $DIRS; do
+		(cd "$PREFIX"; $GCOV $ARGS -o "$i" "$i"/*.gcno)
+	done
+
+	mv "$PREFIX"/*.gcov "$PREFIX/.gcov"
+
+	echo /bin/bash <(curl -s https://codecov.io/bash) -f "$PREFIX/.gcov/*.gcov" -X gcov
 fi
 
-rm -rf   "$PREFIX/.gcov"
-mkdir -p "$PREFIX/.gcov"
-
-DIRS=$(find "$PREFIX" -type f -name \*.gcno -printf "%h\n" | sort -u)
-
-for i in $DIRS; do
-	(cd "$PREFIX"; $GCOV $ARGS -o "$i" "$i"/*.gcno)
-done
-
-mv "$PREFIX"/*.gcov "$PREFIX/.gcov"
-
-echo /bin/bash <(curl -s https://codecov.io/bash) -f "$PREFIX/.gcov/*.gcov" -X gcov
