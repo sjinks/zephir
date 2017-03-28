ece2e63e66d435297ef7defd2924fb7c51cbc4ec
Volodymyr Kolesnykov <volodymyr@wildwolf.name>
2017-03-29 01:45:06 +0300

    codecov.io integration

---
+++ unit-tests/ci/after_success.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+PREFIX=$(readlink -enq "$(dirname $0)/../../")
+
+if [ "$CC" = "gcc" -o "x$CC" = "x" ]; then
+	GCOV=gcov
+	ARGS="-s \"$PREFIX\" -ablp"
+else
+	GCOV=llvm-cov
+	ARGS="gcov -a -b -l -p"
+fi
+
+rm -rf   "$PREFIX/.gcov"
+mkdir -p "$PREFIX/.gcov"
+
+DIRS=$(find "$PREFIX" -type f -name \*.gcno -printf "%h\n" | sort -u)
+
+for i in $DIRS; do
+	(cd "$PREFIX"; $GCOV $ARGS -o "$i" "$i"/*.gcno)
+done
+
+mv "$PREFIX"/*.gcov "$PREFIX/.gcov"
+
+/bin/bash <(curl -s https://codecov.io/bash) -f "$PREFIX/.gcov/*.gcov" -X gcov
